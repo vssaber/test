68adf2825db6af61391b951568b0a6f490524079
vssaber <1391526849@qq.com>
2020-07-08 22:23:28 +0800

    dev/test

---
+++ _test_modules/_test_dict/_test_dict_get.py
@@ -0,0 +1,11 @@
+d1 = {"name": "pengjian", "age": 26}
+
+name = d1["name"]
+
+print(name)
+
+
+# gender = d1["gender"]
+
+gender_get = d1.get("gender")
+print(gender_get)
---
+++ _test_modules/_test_wrap/_test_01.py
@@ -0,0 +1,54 @@
+instances = {}
+
+
+def singleton(cls):
+    def get_instance(*args, **kw):
+        cls_name = cls.__name__
+        print("=====1=====")
+        if not cls_name in instances:
+            print("====2====")
+            instance = cls(args, **kw)
+            instances[cls_name] = instance
+        return instances[cls_name]
+    return get_instance
+
+
+@singleton
+class User:
+    _instance = None
+
+    def __init__(self, name):
+        print("====3====")
+        self.name = name
+
+
+u1 = User("pengjian")
+u1.age = 20
+u2 = User("pengjian2")
+print(u2.age)
+print(u1.name)
+print(u1 is u2)
+
+
+from functools import update_wrapper
+
+
+WRAPPER_ASSIGNMENTS = {"__module__", "__name__", "__qualname__", "__doc__", "__annotations__"}
+
+
+def wrapper(func):
+    def inner_func():
+        pass
+
+
+    update_wrapper(inner_func, func)
+
+    return inner_func
+
+@wrapper
+def wrapped():
+    pass
+
+print("dev")
+
+print(wrapped.__name__)
