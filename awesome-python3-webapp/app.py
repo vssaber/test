ebe301cdeb56a4d63b4a3b3037e2a9a16fb94c1c
vssaber <1391526849@qq.com>
2020-12-03 23:10:37 +0800

    update

--- awesome-python3-webapp/app.py
+++ awesome-python3-webapp/app.py
@@ -1,24 +1,23 @@
-import logging;logging.basicConfig(level=logging.INFO)
+import logging; logging.basicConfig(level=logging.INFO)
 
-import asyncio,os,json,time
+import asyncio, os, json, time
 from datetime import datetime
 
-from aiohttp import web, web_runner
+from aiohttp import web
 
-
-routes = web.RouteTableDef()
-
-
-@routes.get('/')
 def index(request):
-    return web.Response(body=b'<h1>Awesome App</h1>', content_type='text/html')
+    return web.Response(body=b'<h1>Awesome</h1>')
 
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
 
-def init():
-    app = web.Application()
-    app.add_routes([web.get('/', index)])
-    logging.info('server started at http://127.0.0.1:9257...')
-    web.run_app(app, host='127.0.0.1', port=9527)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
 
-
-init()
+print()
